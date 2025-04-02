f465c2d63c618f8fc7f2bc096ccb80baedae20e8
Cesar <cesarnsingi@hotmail.com>
2025-04-02 09:45:51 +0200

    highlight web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Powerful Digital Transformation Services | Imenso Software",
+  title: "Digital Transformation Services | MyEma",
   description: "We are a full stack technology consulting agency specializing in web & mobile application development, Data Analytics, Cloud and DevOps and Designing Services.",
 };
 

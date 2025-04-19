5162af41e9de7a2104595f5bae3fd5f61dafc382
Cesar <cesarnsingi@hotmail.com>
2025-04-19 01:16:14 +0100

    fix instagram link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
   const socialLinks = [
     { icon: <Linkedin className="w-5 h-5" />, href: "https://www.linkedin.com/company/myema-ao/" },
     { icon: <Facebook className="w-5 h-5" />, href: "https://www.facebook.com/profile.php?id=100070673807098" },
-    { icon: <Instagram className="w-5 h-5" />, href: "https://www.instagram.com/myema.ao/" },
+    { icon: <Instagram className="w-5 h-5" />, href: "https://www.instagram.com/ema_aoa/" },
   ];
 
   return (
